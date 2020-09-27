cb0d2ebf0cb0880a12e1a7d5b69b917cbb550a5c
Fatih <contact@fatihcan.dev>
2020-09-27 12:01:21 +0300

    added search component

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Text, View } from "react-native";
-import BoxCenter from "../../components/BoxCenter";
+import Box from "../../components/Box";
 import Button from "../../components/Button";
 import { Logo } from "../../components/icons";
+import SearchBox from "../../components/SearchBox";
 
 const Home = ({ navigation }) => {
   return (
-    <BoxCenter>
+    <Box>
       <Button mb={10} onPress={() => navigation.navigate("Detail")}>
         <Text>Go to detail</Text>
       </Button>
       <Logo width={120} color="red" />
-    </BoxCenter>
+      <Box p={10}>
+        <SearchBox />
+      </Box>
+    </Box>
   );
 };
 
