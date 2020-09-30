d54fa65f2ac09982292cb55298d79c8fc00fe7ca
Fatih <contact@fatihcan.dev>
2020-09-30 21:25:36 +0300

    add animation on search focus, add card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Box from "../Box";
+import Button from "../Button";
+import Text from "../Text";
+
+const Card = ({ title, desc, type, onPress, ...props }) => {
+  return (
+    <Box {...props}>
+      <Text color="textLight" mb={10}>
+        {type}
+      </Text>
+      <Button
+        py={16}
+        px={12}
+        bg="white"
+        borderRadius="normal"
+        onPress={onPress}
+      >
+        <Box flex={1} borderLeftWidth={3} borderLeftColor="light" pl={12}>
+          <Text fontSize={18} fontWeight="bold">
+            {title}
+          </Text>
+          <Text mt={8} color="textMedium" fontSize={14}>
+            {desc}
+          </Text>
+        </Box>
+      </Button>
+    </Box>
+  );
+};
+
+export default Card;
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,46 +1,120 @@
-import React, { useCallback, useState } from "react";
-import { Text, StatusBar } from "react-native";
-import ImageBackground from "react-native/Libraries/Image/ImageBackground";
+import React, { useCallback, useRef, useState } from "react";
+import {
+  Text,
+  StatusBar,
+  Animated,
+  ImageBackground,
+  FlatList
+} from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import Box from "../../components/Box";
 import { Logo } from "../../components/icons";
 import SearchBox from "../../components/SearchBox";
+import Card from "../../components/Card";
 import bg from "../../../assets/bg-red.jpg";
 
-const Home = () => {
-  const [isFocusChanged, setFocusChanged] = useState(false);
+const DATA = [
+  {
+    id: "1",
+    title: "on para",
+    desc: "çok az (para).",
+    type: "Bir Deyim"
+  },
+  {
+    id: "2",
+    title: "siyem siyem ağlamak",
+    desc: "hafif hafif, ince ince, durmadan gözyaşı dökmek.",
+    type: "Bir Deyim - Atasözü"
+  }
+];
+
+const Home = ({ navigation }) => {
+  const heroHeight = useRef(new Animated.Value(285)).current;
+  const [searchFocused, setSearchFocused] = useState(false);
+
+  const handleFocusChange = useCallback(() => {
+    if (searchFocused) {
+      Animated.timing(heroHeight, {
+        toValue: 100,
+        duration: 350,
+        useNativeDriver: false
+      }).start();
+    } else {
+      Animated.timing(heroHeight, {
+        toValue: 285,
+        duration: 350,
+        useNativeDriver: false
+      }).start();
+    }
+  }, [heroHeight, searchFocused]);
 
   useFocusEffect(
     useCallback(() => {
-      StatusBar.setBarStyle("default");
-    }, [])
+      StatusBar.setBarStyle(searchFocused ? "dark-content" : "light-content");
+    }, [searchFocused])
   );
 
   return (
     <Box flex={1}>
-      <Box position="relative" zIndex={1} height={isFocusChanged ? 0 : 285}>
+      <Box
+        as={Animated.View}
+        position="relative"
+        zIndex={1}
+        height={heroHeight}
+      >
+        {!searchFocused && (
+          <Box
+            as={ImageBackground}
+            source={bg}
+            style={{
+              width: "100%",
+              height: "100%"
+            }}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Box flex={1} alignItems="center" justifyContent="center">
+              <Logo color="white" />
+            </Box>
+          </Box>
+        )}
         <Box
-          as={ImageBackground}
-          source={bg}
-          style={{
-            width: "100%",
-            height: "100%"
-          }}
-          alignItems="center"
-          justifyContent="center"
+          p={16}
+          width="100%"
+          position="absolute"
+          left={0}
+          bottom={searchFocused ? 0 : -40}
         >
-          <Box flex={1} alignItems="center" justifyContent="center">
-            <Logo color="red" color="white" />
-          </Box>
-          <Box p={16} width="100%" mb={-40}>
-            <SearchBox onFocusChanged={status => setFocusChanged(status)} />
-          </Box>
+          <SearchBox
+            onFocusChanged={(status) => {
+              setSearchFocused(status);
+              handleFocusChange();
+            }}
+          />
         </Box>
       </Box>
-      <Box flex={1} bg="white" pt={26}>
-        <Box p={30} flex={1}>
-          <Text>Hello</Text>
-        </Box>
+      <Box flex={1} bg="softRed" pt={searchFocused ? 0 : 26}>
+        {searchFocused ? (
+          <Box p={30} flex={1}>
+            <Text>Geçmiş</Text>
+          </Box>
+        ) : (
+            <Box px={16} py={40} flex={1}>
+              <FlatList
+                data={DATA}
+                renderItem={({ item }) => (
+                  <Card
+                    title={item.title}
+                    desc={item.desc}
+                    type={item.type}
+                    onPress={() => navigation.navigate("Detail")}
+                    my={20}
+                  />
+                )}
+                keyExtractor={(item) => item.id}
+              />
+            </Box>
+          )}
       </Box>
     </Box>
   );
