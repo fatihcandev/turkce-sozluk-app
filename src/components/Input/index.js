9303d211be6e654262b5945410a9b88222ff9856
Fatih <contact@fatihcan.dev>
2020-09-30 13:00:23 +0300

    add shadow prop

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,12 +6,13 @@ import {
   color,
   size,
   space,
+  shadow,
   borderRadius
 } from "styled-system";
 import theme from "../../utils/theme";
 
 const Input = styled(TextInput).attrs(props => ({
   placeholderTextColor: theme.colors[props.placeholderTextColor]
-}))(compose(typography, color, size, space, borderRadius));
+}))(compose(typography, color, size, space, borderRadius, shadow));
 
 export default Input;
