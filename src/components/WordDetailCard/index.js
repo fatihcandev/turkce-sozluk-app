0b8d7c706973bd00f584bb0a1db05e9732f3faea
Fatih <contact@fatihcan.dev>
2020-10-03 17:35:59 +0300

    detail page and add word detail card component

---
+++ src/components/WordDetailCard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const WordDetailCard = () => {
+  return <View />;
+};
+
+export default WordDetailCard;
--- src/views/detail/index.js
+++ src/views/detail/index.js
@@ -1,19 +1,46 @@
 import React, { useCallback } from "react";
-import { Text, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import SafeAreaView from "react-native-safe-area-view";
 import Box from "../../components/Box";
+import Text from "../../components/Text";
+import ActionButton from "../../components/ActionButton";
+import theme from "../../utils/theme";
 
 const DetailView = () => {
   useFocusEffect(
     useCallback(() => {
-      StatusBar.setBarStyle("light-content");
+      StatusBar.setBarStyle("dark-content");
     }, [])
   );
 
   return (
-    <Box as={SafeAreaView} flex={1}>
-      <Text>DetailView</Text>
+    <Box as={SafeAreaView} flex={1} bg="softRed" p={16}>
+      <Box>
+        <Text fontSize={32} fontWeight="bold">
+          Kalem
+        </Text>
+        <Text color="textLight" mt={6}>
+          Arapça kalem
+        </Text>
+      </Box>
+      <Box flexDirection="row" mt={24} justifyContent="space-between">
+        <Box flexDirection="row">
+          <ActionButton
+            iconType="voice"
+            color={theme.colors.textDark}
+            mr={12}
+          />
+          <ActionButton
+            iconType="bookmark"
+            color={theme.colors.red}
+            iconFillColor={theme.colors.red}
+          />
+        </Box>
+        <ActionButton iconType="hand" color={theme.colors.textLight}>
+          Türk İşaret Dili
+        </ActionButton>
+      </Box>
     </Box>
   );
 };
