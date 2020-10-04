92fca6606d6e8ee8a3ed9bd1f3657d65ed6abd3c
Fatih <contact@fatihcan.dev>
2020-10-04 13:09:06 +0300

    add word detail card to detail page

--- src/components/WordDetailCard/index.js
+++ src/components/WordDetailCard/index.js
@@ -1,8 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
+import Box from "../Box";
+import Text from "../Text";
 
-const WordDetailCard = () => {
-  return <View />;
+const WordDetailCard = ({
+  number,
+  title,
+  desc,
+  quote,
+  hasBorder,
+  ...props
+}) => {
+  return (
+    <Box
+      px={12}
+      py={20}
+      {...props}
+      borderColor="light"
+      borderBottomWidth={hasBorder ? 1 : 0}
+    >
+      <Box flexDirection="row" alignItems="center">
+        <Text color="textLight" fontSize={14} mr={10}>
+          {number}
+        </Text>
+        <Text color="red">{title}</Text>
+      </Box>
+      <Box pl={18}>
+        <Text color="textDark" fontSize={14} fontWeight="bold" mt={10}>
+          {desc}
+        </Text>
+        <Text color="textLight" fontSize={14} mt={15}>
+          {quote}
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default WordDetailCard;
--- src/views/detail/index.js
+++ src/views/detail/index.js
@@ -1,10 +1,11 @@
 import React, { useCallback } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, ScrollView } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import SafeAreaView from "react-native-safe-area-view";
 import Box from "../../components/Box";
 import Text from "../../components/Text";
 import ActionButton from "../../components/ActionButton";
+import WordDetailCard from "../../components/WordDetailCard";
 import theme from "../../utils/theme";
 
 const DetailView = () => {
@@ -15,20 +16,20 @@ const DetailView = () => {
   );
 
   return (
-    <Box as={SafeAreaView} flex={1} bg="softRed" p={16}>
-      <Box>
-        <Text fontSize={32} fontWeight="bold">
-          Kalem
-        </Text>
-        <Text color="textLight" mt={6}>
-          Arapça kalem
-        </Text>
-      </Box>
-      <Box flexDirection="row" mt={24} justifyContent="space-between">
-        <Box flexDirection="row">
+    <Box as={SafeAreaView} flex={1} bg="softRed">
+      <Box as={ScrollView} p={16}>
+        <Box>
+          <Text fontSize={32} fontWeight="bold">
+            Kalem
+          </Text>
+          <Text color="textLight" mt={6}>
+            Arapça kalem
+          </Text>
+        </Box>
+        <Box flexDirection="row" mt={24}>
           <ActionButton
             iconType="voice"
-            color={theme.colors.textDark}
+            color={theme.colors.textLight}
             mr={12}
           />
           <ActionButton
@@ -36,10 +37,37 @@ const DetailView = () => {
             color={theme.colors.red}
             iconFillColor={theme.colors.red}
           />
+          <Box ml="auto">
+            <ActionButton iconType="hand" color={theme.colors.textLight}>
+              <Text color={theme.colors.textLight} fontWeight="bold">
+                Türk İşaret Dili
+              </Text>
+            </ActionButton>
+          </Box>
         </Box>
-        <ActionButton iconType="hand" color={theme.colors.textLight}>
-          Türk İşaret Dili
-        </ActionButton>
+        <Box mt={30} bg="white" borderRadius="normal">
+          <WordDetailCard
+            number={1}
+            title="İSİM"
+            desc="Yazma, çizme vb. işlerde kullanılan çeşitli biçimlerde araç:"
+            quote={`"Kâğıt, kalem, mürekkep, hepsi masanın üstündedir."- Falih Rıfkı Atay`}
+            hasBorder
+          />
+          <WordDetailCard
+            number={2}
+            title="İSİM"
+            desc="Resmî kuruluşlarda yazı işlerinin görüldüğü yer:"
+            quote={`"Kalemindeki odacıya aylığını kırdırırmış." - Sermet Muhtar Alus`}
+            hasBorder
+          />
+          <WordDetailCard
+            number={2}
+            title="İSİM"
+            desc="Resmî kuruluşlarda yazı işlerinin görüldüğü yer:"
+            quote={`"Kalemindeki odacıya aylığını kırdırırmış." - Sermet Muhtar Alus`}
+            hasBorder
+          />
+        </Box>
       </Box>
     </Box>
   );
